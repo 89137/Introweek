bf87a00f45775de90c7e489277e36440a4bde88d
Belljahh <89137@glr.nl>
2023-09-01 13:08:54 +0200

    Added email system

--- Introweek/Js/Script.js
+++
@@ -1,7 +0,0 @@
-function Form() {
-  location.replace("../Index.html")
-}
-
-function change(element) {
-  element.classList.toggle("fullsize")
-}
--- Js/Script.js
+++ Js/Script.js
@@ -1,6 +1,7 @@
-function Form() {
+function sendEmail() {
   location.replace("../Index.html")
 }
+
 
 function change(element) {
   element.classList.toggle("fullsize")
